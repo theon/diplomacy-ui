4611f20c1b999790cb5c81950d5942f183cac8e8
Ian Forsey <forsey@gmail.com>
2011-10-06 20:46:07 +0000

    Got angularJS binding to SVG, whilst still being able to use the jQuery SVG plugin to get stuff like aspect ratio (unfortunately on an invisible copy of the SVG :( as angularJS and jQuery SVG need the SVG loading into the DOM in different ways)

--- js/map.js
+++ js/map.js
@@ -1,43 +1,60 @@
 $(document).ready(function() {
+   
+   //function mapLoad() {
     
-    $("#map").svg({
+    console.log("woooo");
+    
+    $("#mapCopy").svg({
         loadURL: "img/map.svg",
-        onLoad: function(svg) {
+        settings: {addTo: false, 
+        changeSize: false},
+        onLoad: function(svgWrapper) {
             
-            var originalWidth = svg._svg.viewBox.baseVal.width;
-            var originalHeight = svg._svg.viewBox.baseVal.height;
+            svg = svgWrapper.root();
             
+            resizeMapWidth(svg);
             
-            $(".army").draggable().bind('mousedown', function(event, ui) {
+            $(window).resize(function() {
+                resizeMapWidth(svg);
+            });
+            
+            //svg.removeAttribute('width');
+            //svg.removeAttribute('height');
+            
+            //$(".army use").draggable().bind('mousedown', function(event, ui) {
                 // bring target to front
-                $(event.target.parentElement).append(event.target);
+                //$(event.target.parentElement).append(event.target);
                 //$(this).find("use").attr("transform", "");
                 
-            }).bind('drag', function(event, ui) {
+            //}).bind('drag', function(event, ui) {
                 // update coordinates manually, since top/left style props don't work on SVG
-
-                var widthRatio = originalWidth / svg._svg.width.baseVal.value;
-                var heightRatio = originalHeight / svg._svg.height.baseVal.value;
-
-console.log(originalHeight + "," + svg._svg.height);
 
                 //console.log(ui);
                 //console.log(event);
                 
-                var use = $(this).find("use");
-                use.attr("x", ui.position.left * widthRatio);
-                use.attr("y", ui.position.top * heightRatio);
-                
-            }).bind('mouseup', function(event, ui) {
-                
-                var use = $(this).find("use");
-                use.attr("x", "");
-                use.attr("y", "");
+               // var use = $(this).find("use");
+              //  console.log(use);
                 
                 
-                use.attr("transform", "&Paris;");
+                //use.attr("x", ui.position.left * svg.widthZoomRatio());
+                //use.attr("y", ui.position.top * svg.heightZoomRatio());
+             //   use.attr("x", ui.position.left * getWidthZoomRatio());
+             //   use.attr("y", ui.position.top * getHeightZoomRatio());
                 
-            });
-        }
+            //}).bind('mouseup', function(event, ui) {
+                
+             //   var use = $(this).find("use");
+             //   use.attr("x", DuiConf.Paris.unitPos.x);
+             //   use.attr("y", DuiConf.Paris.unitPos.y);
+                
+            //});
+       }
     });
 });
+
+function resizeMapWidth(svg) {
+    var map = $("#map");
+    map.width(map.height() * svg.apectRatio());
+    console.log(svg.apectRatio());
+    //map.width(map.height() * getViewBoxAspectRatio());
+}
